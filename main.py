a11baa5a1e2c243c18a82218f2d4a8cb814f1401
23f3003526 <23f3003526@ds.study.iitm.ac.in>
2026-07-11 12:15:48 +0530

    Change environment variable for API key

--- main.py
+++ main.py
@@ -184,7 +184,7 @@ async def answer_image(
 ):
     try:
         # Read the Gemini API key from Render.
-        api_key = os.getenv("AQ.Ab8RN6JME4KslbyGENlzmEHrgPU9yKe0cnV073culXB4vzDnGg")
+        api_key = os.getenv("Gemini_API_Key")
 
         if not api_key:
             return JSONResponse(
